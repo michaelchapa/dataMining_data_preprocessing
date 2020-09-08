c0f700e09595278ca1e5d9b7482682031e7ef82d
Michael Chapa <michaelchapa1@gmail.com>
2020-09-07 19:59:26 -0700

    Created first Binning method

--- dataMining_binning_hw2.py
+++ dataMining_binning_hw2.py
@@ -1,16 +1,26 @@
 import pandas as pd
 import numpy as np
 
-# Purpose - Smooths data using DataFrame cut() and qcut() functions
-# Parameter - DataFrame
-# Returns - DataFrame
-def binData(data, depth):
+# Purpose - Smooths data using DataFrame cut()
+# Parameters:
+#   data - DataFrame - to be smoothed
+#   depth - Integer - the number of equal length bins
+# Returns: 
+#   None
+# Comments: Changing pd.cut(labels = False) gives cleaner output
+def bin_Means(data, depth):
+    data = data['F'] # Creates Series
+    data = data.sort_values()
+    binValues, bins = pd.cut(data.array, bins = depth, \
+                labels = False, retbins = True)
+        
+    print("The respective bin for each value of attribute: \n", binValues, "\n")
+    print("The computed specified bins: \n", bins)
+    
+def bin_Boundaries(data, depth):
     data = data['F']
-    array = data.array
-    out = pd.cut(array, depth)
-    
-    print(out)
-    
+    data = data.sort_values()
+    binValues, bins = pd.cut
     
 ########################### MAIN ########################################
 data = pd.read_csv('https://raw.githubusercontent.com/michaelchapa' \
@@ -18,4 +28,4 @@ data = pd.read_csv('https://raw.githubusercontent.com/michaelchapa' \
 data = data[data.columns[1:]] # remove redundant index column
 
 
-binData(data, 100) # k = 100
+bin_Means(data, 100) # k = 100
